abf2726f463de052f9adfab84c765fe8f4fc13b6
RaikiWatanabe <k021g1324@g.neec.ac.jp>
2022-04-14 15:14:31 +0900

    音声まで

--- scene/GameScene.h
+++ scene/GameScene.h
@@ -43,6 +43,27 @@ class GameScene {
 	void Draw();
 
   private: // メンバ変数
+	//テクスチャハンドル
+	uint32_t textureHandle_ = 0;
+	//スプライト
+	Sprite* sprite_ = nullptr;
+
+	//3Dモデル
+	Model* model_ = nullptr;
+
+	//ワールドトランスフォーム
+	WorldTransform worldTransform_;
+	//ビュープロジェクション
+	ViewProjection viewProjection_;
+
+	//サウンドデータハンドル
+	uint32_t soundDataHandle_ = 0;
+
+	//音声再生ハンドル
+	uint32_t voiceHandle_ = 0;
+	//値を表示したい変数
+	int32_t value_ = 0;
+
 	DirectXCommon* dxCommon_ = nullptr;
 	Input* input_ = nullptr;
 	Audio* audio_ = nullptr;
