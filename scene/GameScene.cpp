790e9dd84290e481997e7261365f6604442e99c7
RaikiWatanabe <k021g1324@g.neec.ac.jp>
2022-04-14 15:46:11 +0900

    テキスト追加

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -6,27 +6,26 @@ using namespace DirectX;
 
 GameScene::GameScene() {}
 
-GameScene::~GameScene() { 
+GameScene::~GameScene() {
 	//デストラクタ
 	delete model_;
 }
 
 void GameScene::Initialize() {
-	
+
 	dxCommon_ = DirectXCommon::GetInstance();
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
 	debugText_ = DebugText::GetInstance();
-
 
 	textureHandle_ = TextureManager::Load("mario.jpg ");
 	model_ = Model::Create();
 
 	// X,Y,Z方向のスケーリングを設定
 	worldTransform_.scale_ = {5.0f, 5.0f, 5.0f};
-	//X,Y,Z軸周りの回転角を設定
+	// X,Y,Z軸周りの回転角を設定
 	worldTransform_.rotation_ = {XM_PI / 4.0f, XM_PI / 4.0f, 0.0f};
-	//X,Y,X軸周りの平行移動を設定
+	// X,Y,X軸周りの平行移動を設定
 	worldTransform_.translation_ = {10.0f, 10.0f, 10.0f};
 
 	//ワールドトランスフォームの初期化
@@ -35,11 +34,38 @@ void GameScene::Initialize() {
 	//ビュープロジェクションの初期化
 	viewProjection_.Initialize();
 };
-	
 
-	
+void GameScene::Update() {
+	std::string strDebugTranslation =std::string("translation:(") + 
+	std::to_string(worldTransform_.translation_.x ) + 
+	std::string(",") +
+	std::to_string(worldTransform_.translation_.y)+ 
+	std::string(",") +
+	std::to_string(worldTransform_.translation_.z)+ 
+	std::string(")");
+	//デバッグテキストの表示
+	debugText_->Print(strDebugTranslation, 50, 50, 1.0f);
 
-void GameScene::Update() {}
+	std::string strDebugRotation = std::string("rotation:(") +
+	std::to_string(worldTransform_.rotation_.x) +
+	std::string(",") +
+	std::to_string(worldTransform_.rotation_.y) +
+	std::string(",") +
+	std::to_string(worldTransform_.rotation_.z) + 
+	std::string(")");
+	//デバッグテキストの表示
+	debugText_->Print(strDebugRotation, 50, 80, 1.0f);
+
+	std::string strDebugScale = std::string("scale:(") +
+	std::to_string(worldTransform_.scale_.x) + 
+	std::string(",") +
+	std::to_string(worldTransform_.scale_.y) +
+	std::string(",") +
+	std::to_string(worldTransform_.scale_.z) +
+	std::string(")");
+	//デバッグテキストの表示
+	debugText_->Print(strDebugScale, 50, 110, 1.0f);
+}
 
 void GameScene::Draw() {
 
@@ -67,7 +93,7 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
-	
+
 	// 3Dモデル描画
 	model_->Draw(worldTransform_, viewProjection_, textureHandle_);
 
@@ -88,7 +114,6 @@ void GameScene::Draw() {
 	//
 	// スプライト描画後処理
 	Sprite::PostDraw();
-
 
 #pragma endregion
 }
