daa64869b48e197c746a155bed97b3a365b6f43b
RaikiWatanabe <k021g1324@g.neec.ac.jp>
2022-04-21 14:59:23 +0900

    親と子供

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -29,26 +29,29 @@ void GameScene::Initialize() {
 	std::uniform_real_distribution<float> rotDist(0.0f, XM_2PI);
 	//乱数範囲(座標用)
 	std::uniform_real_distribution<float> posDist(-10.0f, 10.0f);
-	for (size_t i = 0; i < _countof(worldTransform_); i++) {
 
-		// X,Y,Z方向のスケーリングを設定
-		worldTransform_[i].scale_ = {1.0f, 1.0f, 1.0f};
-		// X,Y,Z軸周りの回転角を設定
-		worldTransform_[i].rotation_ = {rotDist(engine), rotDist(engine), rotDist(engine)};
-		// X,Y,X軸周りの平行移動を設定
-		worldTransform_[i].translation_ = {posDist(engine), posDist(engine), posDist(engine)};
+	//// X,Y,Z方向のスケーリングを設定
+	// worldTransform_[i].scale_ = {1.0f, 1.0f, 1.0f};
+	//// X,Y,Z軸周りの回転角を設定
+	// worldTransform_[i].rotation_ = {rotDist(engine), rotDist(engine), rotDist(engine)};
+	//// X,Y,X軸周りの平行移動を設定
+	// worldTransform_[i].translation_ = {posDist(engine), posDist(engine), posDist(engine)};
 
-		//ワールドトランスフォームの初期化
-		worldTransform_[i].Initialize();
-	}
-	//カメラ垂直方向視野角を設定
-	viewProjection_.fovAngleY = XMConvertToRadians(10.0f);
+	//親(0番)
+	worldTransform_[0].Initialize();
+	//子
+	worldTransform_[1].translation_ = {0, 4.5f, 0};
+	worldTransform_[1].parent_ = &worldTransform_[0];
+	worldTransform_[1].Initialize();
+
+	////カメラ垂直方向視野角を設定
+	//viewProjection_.fovAngleY = XMConvertToRadians(5.0f);
 	////アスペクト比を設定
-	//viewProjection_.aspectRatio = 1.0f;
-	//ニアクリップ距離を設定
-	viewProjection_.nearZ = 52.0f;
-	//ファークリップ距離を設定
-	viewProjection_.farZ = 53.0f;
+	// viewProjection_.aspectRatio = 1.0f;
+	////ニアクリップ距離を設定
+	// viewProjection_.nearZ = 52.0f;
+	////ファークリップ距離を設定
+	// viewProjection_.farZ = 53.0f;
 	//ビュープロジェクションの初期化
 	viewProjection_.Initialize();
 };
@@ -111,7 +114,7 @@ void GameScene::Update() {
 	//		debugText_->SetPos(50, 50);
 	//		debugText_->Printf(
 	//		  "eye:(%f,%f,%f)", viewProjection_.eye.x, viewProjection_.eye.y,
-	//viewProjection_.eye.z);
+	// viewProjection_.eye.z);
 	//	}
 	//
 	//	//注視点移動処理
@@ -166,38 +169,71 @@ void GameScene::Update() {
 	//		debugText_->Printf(
 	//		  "up(%f,%f,%f)", viewProjection_.up.x, viewProjection_.up.y, viewProjection_.up.z);
 	//	}
-	// Fov変更処理
+
+	//	// Fov変更処理
+	//	{
+	//		//上キーで視野角が広がる
+	//		if (input_->PushKey(DIK_W)) {
+	//			viewProjection_.fovAngleY += 0.01f;
+	//			viewProjection_.fovAngleY = min(viewProjection_.fovAngleY, XM_PI);
+	//		} else if (input_->PushKey(DIK_S)) {
+	//			viewProjection_.fovAngleY -= 0.01f;
+	//			viewProjection_.fovAngleY = max(viewProjection_.fovAngleY, 0.01f);
+	//		}
+	//		//行列の再計算
+	//		viewProjection_.UpdateMatrix();
+	//		//デバッグ用表示
+	//		debugText_->SetPos(50, 110);
+	//		debugText_->Printf("fovAngleY(Degree):%f",
+	// XMConvertToDegrees(viewProjection_.fovAngleY));
+	//	}
+	//
+	//	//クリップ垂直変更処理
+	//	{
+	//		//上下キーでニアクリップ距離を増減
+	//		if (input_->PushKey(DIK_UP)) {
+	//			viewProjection_.nearZ += 0.1f;
+	//		} else if (input_->PushKey(DIK_DOWN)) {
+	//			viewProjection_.nearZ -= 0.1f;
+	//		}
+	//		//行列の再計算
+	//		viewProjection_.UpdateMatrix();
+	//
+	//		//デバッグ用表示
+	//		debugText_->SetPos(50, 130);
+	//		debugText_->Printf("nearZ:%f", viewProjection_.nearZ);
+	//	}
+
+	//キャラクター移動処理
 	{
-		//上キーで視野角が広がる
-		if (input_->PushKey(DIK_W)) {
-			viewProjection_.fovAngleY += 0.01f;
-			viewProjection_.fovAngleY = min(viewProjection_.fovAngleY, XM_PI);
-		} else if (input_->PushKey(DIK_S)) {
-			viewProjection_.fovAngleY -= 0.01f;
-			viewProjection_.fovAngleY = max(viewProjection_.fovAngleY, 0.01f);
+		//キャラクターの移動ベクトル
+		XMFLOAT3 move = {0, 0, 0};
+		
+		//キャラクターの移動速さ
+		const float kCharacterSpeed = 0.2f;
+
+		//押した方向で移動ベクトルを変更
+		if (input_->PushKey(DIK_LEFT)) {
+			move = {-kCharacterSpeed, 0, 0};
+		} else if (input_->PushKey(DIK_RIGHT)) {
+			move = {kCharacterSpeed, 0, 0};
 		}
-		//行列の再計算
-		viewProjection_.UpdateMatrix();
+		//注視点移動(ベクトルの加算)
+		worldTransform_[PartId::Root].translation_.x += move.x;
+		worldTransform_[PartId::Root].translation_.y += move.y;
+		worldTransform_[PartId::Root].translation_.z += move.z;
+		
 		//デバッグ用表示
-		debugText_->SetPos(50, 110);
-		debugText_->Printf("fovAngleY(Degree):%f", XMConvertToDegrees(viewProjection_.fovAngleY));
+		debugText_->SetPos(50, 150);
+		debugText_->Printf(
+		  "Root:(%f,%f,%f)", worldTransform_[PartId::Root].translation_.x,
+		  worldTransform_[PartId::Root].translation_.y,
+		  worldTransform_[PartId::Root].translation_.z);
 	}
 
-	//クリップ垂直変更処理
-	{
-		//上下キーでニアクリップ距離を増減
-		if (input_->PushKey(DIK_UP)) {
-			viewProjection_.nearZ += 0.1f;
-		} else if (input_->PushKey(DIK_DOWN)) {
-			viewProjection_.nearZ -= 0.1f;
-		}
-		//行列の再計算
-		viewProjection_.UpdateMatrix();
-
-		//デバッグ用表示
-		debugText_->SetPos(50, 130);
-		debugText_->Printf("nearZ:%f", viewProjection_.nearZ);
-	}
+	worldTransform_[0].UpdateMatrix();
+	worldTransform_[1].UpdateMatrix();
+	
 }
 
 void GameScene::Draw() {
@@ -228,9 +264,8 @@ void GameScene::Draw() {
 	/// </summary>
 
 	// 3Dモデル描画
-	for (size_t i = 0; i < _countof(worldTransform_); i++) {
-		model_->Draw(worldTransform_[i], viewProjection_, textureHandle_);
-	}
+	model_->Draw(worldTransform_[0], viewProjection_, textureHandle_);
+	model_->Draw(worldTransform_[1], viewProjection_, textureHandle_);
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
 #pragma endregion
--- scene/GameScene.h
+++ scene/GameScene.h
@@ -18,7 +18,19 @@ class GameScene {
 
   public: // メンバ関数
 
-	
+	//パーツID
+	  enum PartId {
+		  Root,		//大元
+		  Spine,	//脊髄
+		  Chest,	//胸
+		  Head,		//頭
+		  ArmL,		//左腕
+		  ArmR,		//右腕
+		  Hip,		//尻
+		  LegL,		//左足
+		  LegR,		//右足
+	};
+
 	/// <summary>
 	/// コンストクラタ
 	/// </summary>
